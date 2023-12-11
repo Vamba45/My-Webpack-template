b0342879ac39563b029ed8ea4e27534b12a9401d
Vamba45 <vambafool@bk.ru>
2023-12-11 17:53:01 +0300

    add declarations

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,11 @@
+import IMAGE from '../assets/img/Observer.jpg';
+import FONT from '../assets/fonts/Whisper-Regular.ttf';
+
 const Footer = () => {
     return(
         <footer>
             It's Footer
+            <img src={IMAGE} alt="" />
         </footer>
     )   
 }
